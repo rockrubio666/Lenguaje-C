ee9bc84b63418e94c36e59da9b7ec2bbf3d49ef7
rockrubio666 <monroyrubioc@gmail.com>
2017-01-24 14:42:27 -0600

    Código reducido

---
+++ Práctica 6/shrink_me.c
@@ -0,0 +1,15 @@
+//Practica 6
+//"Hackea" este programa
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+	unsigned int var1,var2,mayor;
+	printf("Ingresa dos enteros sin signo(maximo %u):",UINT_MAX);
+	scanf("%d %d",&var1,&var2);
+	if(var1>var2)
+		mayor=var1;
+	else if (var2>var1)
+		mayor=var2;
+	printf("El mayor es %d\n",mayor);
+}
