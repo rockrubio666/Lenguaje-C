ec6641f7b7a3d9d5ffee97e4f4cab56537443bff
rockrubio666 <monroyrubioc@gmail.com>
2017-01-24 00:23:09 -0600

    Límites

---
+++ Práctica 2/limites.c
@@ -0,0 +1,13 @@
+//Monroy Rubio Cristian Alexis
+#include <limits.h>
+
+
+int main()
+{
+	printf("%d\n",UCHAR_MAX);
+	printf("%ld\n", LONG_MAX);
+	printf("%d\n",SHRT_MAX);
+	printf("%u\n",UINT_MAX);
+	printf("%d\n",USHRT_MAX);
+	return 0;
+}
